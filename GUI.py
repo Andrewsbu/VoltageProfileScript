cc68efc8a4b0a9b4d3c3f01e1ae3229e4f6fa48f
Andrew <andrew.nicoll@stonybrook.edu>
2022-12-01 15:47:05 -0500

    Coulombic Efficiency & GUI Work

--- GUI.py
+++ GUI.py
@@ -6,3 +6,34 @@ Created on Wed Nov 30 14:34:50 2022
 """
 
 import tkinter as tk
+from tkinter import ttk
+from tkinter import filedialog as fd
+from tkinter import *
+from tkinter.messagebox import showinfo
+
+
+root = tk.Tk()
+root.title('Echem Plotter')
+root.geometry('800x600')
+
+def browsefile():
+    
+    selectfile = fd.askopenfilename(title='Open file', initialdir='/',\
+                                    filetype=(('Excel files',('.*xlsx','*.xls')),\
+                                              ('All files','*.')))
+    label = Label(root,text=f'Currnet File: {selectfile}')
+    label.pack(anchor=tk.N)
+    
+browse_button = ttk.Button(root,text='Browser',command=browsefile)
+browse_button.pack(expand=True)
+
+
+
+
+
+
+
+
+
+
+root.mainloop()
